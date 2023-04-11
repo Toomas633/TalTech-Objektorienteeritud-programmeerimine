e8763554575f69ef4817213b2a991957b81c8ebe
Toomas <toomas.kirsing@gmail.com>
2023-04-11 13:22:26 +0300

    Tund 8

---
+++ kirsing_8/src/com/example/App.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("ui.fxml"));
+        primaryStage.setTitle("Kodutöö 8");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
